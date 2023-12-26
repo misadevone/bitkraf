1da1570c609499545fc7b046402b7f92dda42d5e
misadevone <152486848+misadevone@users.noreply.github.com>
2023-12-27 01:06:21 +0800

    Add files via upload

--- dump/script.js
+++ dump/script.js
@@ -3,15 +3,20 @@
 var wrapMain = document.getElementById("wrap-main");
 
 document.getElementById("btn-tgl-width").addEventListener("click", toggleWidth);
-document.getElementById("btn-tgl-all").addEventListener("click", toggleAdvanceSuperKontol);
+document.getElementById("btn-tgl-all").addEventListener("click", toggleAdvanceSuper);
+document.getElementById("btn-tgl-allbutwithnotch").addEventListener("click", toggleWidthNotch);
 
 
 
-
-function toggleAdvanceSuperKontol() {
+function toggleAdvanceSuper() {
     toggleWidth();
 }
 
 function toggleWidth() {
     wrapMain.classList.toggle("advahanced");
+}
+
+function toggleWidthNotch() {
+    wrapMain.classList.toggle("advahancedtol");
+    wrapMain.parentNode.style.background="black";
 }
