7b40b35f6e2f04bfd5e88e2e49ac180f6e212f36
misadevone <152486848+misadevone@users.noreply.github.com>
2023-12-29 13:52:44 +0800

    Add files via upload

--- dump/script.js
+++ dump/script.js
@@ -3,7 +3,13 @@ const wrapMain = document.getElementById("wrap-main");
 
 btnTglWidth.addEventListener("click", toggleWidth);
 
+toggleWidth();
 
 function toggleWidth() {
     wrapMain.classList.toggle("full-width");
+}
+
+function tglSide() {
+    document.getElementById("flm1").classList.toggle("full-width");
+    // wrapMain.classList.toggle("full-width");
 }
